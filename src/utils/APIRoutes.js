258d1f48a8d1ccc9d3e456a9a1d57e99779c65f0
Kumar Sanyam <142786272+kumarsanyam01@users.noreply.github.com>
2024-05-24 02:04:03 +0530

    Update APIRoutes.js

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host="https://chat-app-e6d5.onrender.com";
+export const host="https://chat-app-1-i3ak.onrender.com";
 export const registerRoute=`${host}/api/auth/register`;
 export const loginRoute=`${host}/api/auth/login`;
 export const setAvatarRoute=`${host}/api/auth/setAvatar`;
